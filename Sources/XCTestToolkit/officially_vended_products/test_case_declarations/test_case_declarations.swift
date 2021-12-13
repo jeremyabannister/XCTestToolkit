12e9e9cb6ae61489a5e872b7e086db9b03f2ea2e
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2021-12-13 16:01:26 +0100

    Added test case declarations for foundation types.

--- Sources/XCTestToolkit/officially_vended_products/test_case_declarations/test_case_declarations.swift
+++ Sources/XCTestToolkit/officially_vended_products/test_case_declarations/test_case_declarations.swift
@@ -5,17 +5,109 @@
 //  Created by Jeremy Bannister on 12/11/21.
 //
 
-///
-public final class XCTestCase_tests: SingleTypeTestCase {
-    public typealias Subject = XCTestCase
-}
+// MARK: - XCTestToolkit -
 
 ///
 public final class SingleTypeTestCase_tests: SingleTypeTestCase {
     public typealias Subject = SingleTypeTestCase
 }
-
-///
 public final class SingleTypeTestCaseProtocol_tests: SingleTypeTestCase {
     public typealias Subject = SingleTypeTestCaseProtocol
+}
+
+// MARK: - XCTest -
+
+///
+public final class XCTestCase_tests: SingleTypeTestCase {
+    public typealias Subject = XCTestCase
+}
+
+// MARK: - Foundation -
+
+/// Concrete Types
+public final class Array_tests: SingleTypeTestCase {
+    public typealias Subject = Array<Int>
+}
+public final class Bool_tests: SingleTypeTestCase {
+    public typealias Subject = Bool
+}
+public final class ClosedRange_tests: SingleTypeTestCase {
+    public typealias Subject = ClosedRange<Int>
+}
+public final class Data_tests: SingleTypeTestCase {
+    public typealias Subject = Data
+}
+public final class Date_tests: SingleTypeTestCase {
+    public typealias Subject = Date
+}
+public final class DateFormatter_tests: SingleTypeTestCase {
+    public typealias Subject = DateFormatter
+}
+public final class Dictionary_tests: SingleTypeTestCase {
+    public typealias Subject = Dictionary<String, Int>
+}
+public final class Dictionary_Keys_tests: SingleTypeTestCase {
+    public typealias Subject = Dictionary_tests.Subject.Keys
+}
+public final class Dictionary_Values_tests: SingleTypeTestCase {
+    public typealias Subject = Dictionary_tests.Subject.Values
+}
+public final class Double_tests: SingleTypeTestCase {
+    public typealias Subject = Double
+}
+public final class Int_tests: SingleTypeTestCase {
+    public typealias Subject = Int
+}
+public final class JSONDecoder_tests: SingleTypeTestCase {
+    public typealias Subject = JSONDecoder
+}
+public final class JSONEncoder_tests: SingleTypeTestCase {
+    public typealias Subject = JSONEncoder
+}
+public final class Optional_tests: SingleTypeTestCase {
+    public typealias Subject = Optional<Int>
+}
+public final class Range_tests: SingleTypeTestCase {
+    public typealias Subject = Range<Int>
+}
+public final class Result_tests: SingleTypeTestCase {
+    public typealias Subject = Result<Int, Error>
+}
+public final class Set_tests: SingleTypeTestCase {
+    public typealias Subject = Set<Int>
+}
+public final class String_tests: SingleTypeTestCase {
+    public typealias Subject = String
+}
+public final class UInt_tests: SingleTypeTestCase {
+    public typealias Subject = UInt
+}
+public final class URL_tests: SingleTypeTestCase {
+    public typealias Subject = URL
+}
+public final class URLComponents_tests: SingleTypeTestCase {
+    public typealias Subject = URLComponents
+}
+public final class UUID_tests: SingleTypeTestCase {
+    public typealias Subject = UUID
+}
+
+/// Protocols
+public final class AdditiveArithmetic_tests: SingleTypeTestCase {
+    public typealias Subject = AdditiveArithmetic
+}
+public final class BinaryInteger_tests: SingleTypeTestCase {
+    public typealias Subject = BinaryInteger
+}
+public final class Decodable_tests: SingleTypeTestCase {
+    public typealias Subject = Decodable
+}
+public final class Encodable_tests: SingleTypeTestCase {
+    public typealias Subject = Encodable
+}
+public final class Numeric_tests: SingleTypeTestCase {
+    public typealias Subject = Numeric
+}
+public final class Sequence_tests: SingleTypeTestCase {
+    public typealias Subject = Sequence
 }
--- Tests/XCTestToolkit-tests/officially_vended_products/test_case_declarations/test_case_declarations_tests.swift
+++ Tests/XCTestToolkit-tests/officially_vended_products/test_case_declarations/test_case_declarations_tests.swift
@@ -5,5 +5,47 @@
 //  Created by Jeremy Bannister on 12/11/21.
 //
 
+// MARK: - XCTestToolkit -
+
+///
+extension SingleTypeTestCase_tests { }
+extension SingleTypeTestCaseProtocol_tests { }
+
+// MARK: - XCTest -
+
 ///
 extension XCTestCase_tests { }
+
+// MARK: - Foundation -
+
+/// Concrete Types
+extension Array_tests { }
+extension Bool_tests { }
+extension ClosedRange_tests { }
+extension Data_tests { }
+extension Date_tests { }
+extension DateFormatter_tests { }
+extension Dictionary_tests { }
+extension Dictionary_Keys_tests { }
+extension Dictionary_Values_tests { }
+extension Double_tests { }
+extension Int_tests { }
+extension JSONDecoder_tests { }
+extension JSONEncoder_tests { }
+extension Optional_tests { }
+extension Range_tests { }
+extension Result_tests { }
+extension Set_tests { }
+extension String_tests { }
+extension UInt_tests { }
+extension URL_tests { }
+extension URLComponents_tests { }
+extension UUID_tests { }
+
+/// Protocols
+extension AdditiveArithmetic_tests { }
+extension BinaryInteger_tests { }
+extension Decodable_tests { }
+extension Encodable_tests { }
+extension Numeric_tests { }
+extension Sequence_tests { }
